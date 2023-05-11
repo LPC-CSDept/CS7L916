00947c148601bbf37c513cad3a7055205cd017db
Kyuwoong Lee <kwooler@gmail.com>
2023-05-11 14:35:13 -0700

    Initial commit

---
+++ main_test.py
@@ -0,0 +1,34 @@
+import main
+import io
+import sys
+import re
+import math
+import types
+
+
+def test_main_1():
+    captureOut = io.StringIO()
+    sys.stdout = captureOut
+    # datastr = 'Kim \n 100 80 70 60\n Bill \n 100 90 80 60 \n Mary \n 90 80 70 100'
+    # sys.stdin = io.StringIO(datastr)
+
+    sys.stdout = sys.__stdout__
+    print('Captured ', captureOut.getvalue())
+    lines = captureOut.getvalue().split('\n')
+    print(lines)
+
+    ret = main.main()
+    assert len(ret) == 4
+    assert ret[0] == 10
+    assert ret[1] == 15
+    assert ret[2] == 25
+    assert ret[3] == 55
+
+    # regex_string = r'[\w,\W]*1'
+    # regex_string += r'[\w,\W]*3'
+    # regex_string += r'[\w,\W]*5'
+    # regex_string += r'[\w,\W]*'
+    # print(regex_string)
+    # res = re.search(regex_string, main.evenlist)
+    # assert res != None
+    # print(res.group())
